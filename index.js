c9996b60c410c128df886aac0d0c5f6edb4c9cad
OndraVicha <77784807+OndraVicha@users.noreply.github.com>
2021-11-08 17:23:29 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const osmy = document.querySelector("#osmy");
+osmy.addEventListener("mouseover", function () {
+    osmy.style.color = "white";
+
+});
+osmy.addEventListener("mouseout", function () {
+    osmy.style.color = "grey";
+});
